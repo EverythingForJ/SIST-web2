434e33b7063469c0204d0af14dbef52b6511a784
JunhwanKim <67085700+EverythingForJ@users.noreply.github.com>
2020-11-17 16:52:54 +0900

    Add files via upload

---
+++ jsp/JSP_Standard_Tag_Library/Member.java
@@ -0,0 +1,27 @@
+package com.example.libs;
+
+public class Member {
+	private String name; // property
+	private int age;
+	private boolean flag;
+	
+	public boolean isFlag() {
+		return flag;
+	}
+	public void setFlag(boolean flag) {
+		this.flag = flag;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+}
